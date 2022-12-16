347de2077ef8c3574144168138da94dfc3e6449c
1715707082 <xy183761.@163.com>
2022-12-16 17:48:29 +0800

    去去去1

--- app/src/main/java/com/a/hexinyu/MainActivity.java
+++ app/src/main/java/com/a/hexinyu/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private String a;
+    private String c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
